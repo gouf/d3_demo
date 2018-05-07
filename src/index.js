2ad4cf43ea7308ca53a687bcbf4d395f27f5196a
gouf <innocent.zero@gmail.com>
2018-05-07 10:24:35 +0900

    Using jQuery and D3 with webpack

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import * as d3 from 'd3'
+import $ from 'jquery'
+
+$(function () {
+  console.log('Hello, Hello.')
+
+  var data = [10, 15, 20, 30, 40, 250, 320]
+  d3.select('.d3')
+    .selectAll('div')
+    .data(data)
+      .enter()
+      .append('div')
+      .style('width', function(d) { return ''.concat(d, 'px') })
+      .text(function(d) { return d });
+})
